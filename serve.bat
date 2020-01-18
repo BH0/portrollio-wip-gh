d7dbae631b2ee0355254717c95c14a11c6677a81
bh0 <bryce13h0@gmail.com>
2020-01-18 09:40:31 +0000

    update serve.bat

--- serve.bat
+++ serve.bat
@@ -1,5 +1,5 @@
 REM Will serve via Github Pages 
-REM may need to create new Github repository with Curl + Github API 
+REM This should be the only file which touches the GH Pages branch 
 git switch -c gh-pages 
 git remote add gh-pages https://github.com/bh0/portrollio-test.git
 git add index.html styles.css custom.css assets .gitignore serve.bat 
