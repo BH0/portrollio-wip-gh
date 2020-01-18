4cadd0f8f7deba9ea4ede88363cc3d47b02c87ac
bh0 <bryce13h0@gmail.com>
2020-01-18 10:08:06 +0000

    serve.bat will not add gh-pages remote

--- serve.bat
+++ serve.bat
@@ -1,7 +1,7 @@
 REM Will serve via Github Pages 
 REM This should be the only file which touches the GH Pages branch 
 git switch -c gh-pages 
-git remote add gh-pages https://github.com/bh0/portrollio-test.git
+REM git remote add gh-pages https://github.com/bh0/portrollio-test.git
 git add index.html styles.css custom.css assets .gitignore serve.bat 
 git commit -m "serve via gh-pages"
 git push origin gh-pages
