49b49ebb5909226a5c5a2578250fcc068c7f4710
bh0 <bryce13h0@gmail.com>
2020-01-18 09:48:49 +0000

    serve via gh-pages

--- assets/scripts.js
+++ assets/scripts.js
@@ -37,3 +37,19 @@ ScrollReveal().reveal('.grid-item');
 ScrollReveal().reveal('.dummy-section');
 
 // More code to come 
+
+        document.querySelectorAll('.grid-item').forEach(gridItem => gridItem.addEventListener('click', e => {
+            let items = [{"src":"https://images.pexels.com/photos/110820/pexels-photo-110820.jpeg?auto=compress&cs=tinysrgb&dpr=1","info":"Junior $40"},{"src":"https://images.pexels.com/photos/1300361/pexels-photo-1300361.jpeg?auto=compress&cs=tinysrgb&dpr=1","info":"Elder $80"},{"src":"https://images.pexels.com/photos/787840/pexels-photo-787840.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","info":"Adult $70"},{"src":"https://images.pexels.com/photos/110820/pexels-photo-110820.jpeg?auto=compress&cs=tinysrgb&dpr=1","info":"Junior $40"},{"src":"https://images.pexels.com/photos/1300361/pexels-photo-1300361.jpeg?auto=compress&cs=tinysrgb&dpr=1","info":"Elder $80"},{"src":"https://images.pexels.com/photos/787840/pexels-photo-787840.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","info":"Adult $70"}]
+            let index = 0; 
+            items.forEach((slide, i) => {
+                if (slide.src == e.target.src) {
+                    index = i; 
+                } 
+            }); 
+            var options = {
+                index: index
+            }
+            var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+            gallery.init(); 
+        })); 
+        
