25279e86567782b4395021f2e2839cadd8faf70a
bh0 <bryce13h0@gmail.com>
2020-01-18 09:53:19 +0000

    no message

--- serve.bat
+++ serve.bat
@@ -5,3 +5,4 @@ git remote add gh-pages https://github.com/bh0/portrollio-test.git
 git add index.html styles.css custom.css assets .gitignore serve.bat 
 git commit -m "serve via gh-pages"
 git push origin gh-pages
+REM may need to change branch (back to master) 
